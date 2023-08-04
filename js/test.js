0fa22bd567de10df432f9070c2b2ca87de27df23
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-08-04 19:43:58 -0400

    adding query
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- js/test.js
+++ js/test.js
@@ -1,1 +1,16 @@
-alert("test")
+$.ajax({
+
+    url : 'https://inventory-service-l7id2vv4oq-ue.a.run.app/data',
+    type : 'GET',
+    // data : {
+    //     'numberOfWords' : 10
+    // },
+    dataType:'json',
+    success : function(data) {              
+        alert('Data: '+data);
+    },
+    error : function(request,error)
+    {
+        alert("Request: "+JSON.stringify(request));
+    }
+});
