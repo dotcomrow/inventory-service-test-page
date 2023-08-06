15bbd0075f45aa9a7442f6a069d0340ef44f444e
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-08-05 21:10:37 -0400

    adding post
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- js/test.js
+++ js/test.js
@@ -1,10 +1,12 @@
 $.ajax({
 
     url : 'https://inventory-service-l7id2vv4oq-ue.a.run.app/data',
-    type : 'GET',
-    // data : {
-    //     'numberOfWords' : 10
-    // },
+    type : 'POST',
+    data : {
+        'account_id' : window.user,
+        'item_id' : '1',
+        'item_description' : 'test'
+    },
     dataType:'json',
     success : function(data) {              
         alert('Data: '+data);
