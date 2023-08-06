22cc1051c08d858f9903ff7d6c3009c9a8d43e05
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-08-05 21:26:20 -0400

    test2
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- js/test.js
+++ js/test.js
@@ -1,9 +1,10 @@
+var params = JSON.parse(localStorage.getItem('oauth2-test-params'));
 $.ajax({
 
-    url : 'https://inventory-service-l7id2vv4oq-ue.a.run.app/data',
+    url : 'https://inventory-service-l7id2vv4oq-ue.a.run.app/data?access_token=' + params['access_token'],
     type : 'POST',
     data : {
-        'account_id' : window.user,
+        'account_id' : window.user['id'],
         'item_id' : '1',
         'item_description' : 'test'
     },
