0ce889fd971c5984363667d727d82eee32c87ed1
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-08-05 22:47:42 -0400

    t2
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- js/test.js
+++ js/test.js
@@ -1,26 +1,28 @@
 var params = JSON.parse(localStorage.getItem('oauth2-test-params'));
 console.log(window.user);
-$.ajax({
+if (window.user) {
+    $.ajax({
 
-    url : 'https://inventory-service-l7id2vv4oq-ue.a.run.app/data',
-    type : 'POST',
-    data : {
-        'account_id' : JSON.parse(window.user)['id'],
-        'item_id' : '1',
-        'item_description' : 'test'
-    },
-    "headers": {
-        "accept": "application/json",
-        "Access-Control-Allow-Origin":"*",
-        "authorization": "Bearer " + params['access_token'],
-    },
-    crossDomain: true,
-    dataType:'json',
-    success : function(data) {              
-        alert('Data: '+data);
-    },
-    error : function(request,error)
-    {
-        alert("Request: "+JSON.stringify(request));
-    }
-});
+        url : 'https://inventory-service-l7id2vv4oq-ue.a.run.app/data',
+        type : 'POST',
+        data : {
+            'account_id' : JSON.parse(window.user)['id'],
+            'item_id' : '1',
+            'item_description' : 'test'
+        },
+        "headers": {
+            "accept": "application/json",
+            "Access-Control-Allow-Origin":"*",
+            "authorization": "Bearer " + params['access_token'],
+        },
+        crossDomain: true,
+        dataType:'json',
+        success : function(data) {              
+            alert('Data: '+data);
+        },
+        error : function(request,error)
+        {
+            alert("Request: "+JSON.stringify(request));
+        }
+    });
+}
